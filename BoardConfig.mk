d766ef5a69779cc00f0778bbb62185127a2e94c5
milaq <micha.laqua@gmail.com>
2012-03-05 16:22:29 +0100

    set c/c++ flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,6 +36,9 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp	
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
 TARGET_NO_BOOTLOADER := true
 
 BOARD_KERNEL_BASE := 0x11800000
