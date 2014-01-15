5a6a2ccb4e760d3d89de3676b988241ef2e81e90
milaq <micha.laqua@gmail.com>
2014-01-15 09:18:12 +0100

    wifi: migrate to bcmdhd

--- device_leo.mk
+++ device_leo.mk
@@ -163,6 +163,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
-$(call inherit-product-if-exists, hardware/broadcom/wlan-legacy/bcm4329/firmware/device-bcm.mk)
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 $(call inherit-product, vendor/htc/leo/leo-vendor.mk)
